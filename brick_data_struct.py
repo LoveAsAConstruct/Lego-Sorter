f72433b31d06031e273c6cd30f56b2c9e955c7cf
Isa (just on my PC) <isaiah.j.murray@gmail.com>
2024-04-13 15:23:24 -0400

    updated brick class

--- brick_data_struct.py
+++ brick_data_struct.py
@@ -1,2 +1,13 @@
 class Brick():
-    def
+    def __init__(id, init_string = None):
+        this.id = id
+        if init_string != None:
+            index = init_string.find(' ')
+            if index != -1: 
+                first_part = init_string[:index]
+                second_part = init_string[index+1:]
+                this.part_number = first_part
+                this.name = second_part
+                return
+        this.part_number = None
+        this.name = None
